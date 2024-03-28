c2d5d4b3ac456574d707a62932a489ba23877ba3
cherkezyan <cherkezyan@itstep.academy>
2024-03-28 19:49:12 +0200

    Создала массив и вывела на консоль

--- MyTcp/Program.cs
+++ MyTcp/Program.cs
@@ -11,6 +11,11 @@ namespace MyTcp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            int[] mas = { 1, 2, 3, 4, 5 };
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine(mas[i]);
+            }
         }
     }
 }
